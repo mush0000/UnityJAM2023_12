6543e426f5ddf696b6a870b19ce367436466deb1
mush0000 <mashroom0825@gmail.com>
2023-12-24 07:32:40 +0900

    リザルト時のスコアに応じた表示切り替え

--- Assets/Scripts/ResultManager.cs
+++ Assets/Scripts/ResultManager.cs
@@ -1,19 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Result : MonoBehaviour
+public class ResultManager : MonoBehaviour
 {
     //このクラスの役割
     //３問回答を終えたらResultシーンへ切り替え
     //スコアに応じた結果を画面に表示
 
     public int resultScores;//ゲームディレクタークラスのスコアを受け取る
-    public string resultRunk;//スコアに応じたランク
-    public string resultComment;//ランクごとのコメント
-    public Sprite resultImage;
+    public GameObject resultRank;//スコアに応じたランク
+    public string rank;
+    public GameObject resultComment;//ランクごとのコメント
+    public string comment;
+    public GameObject resultImage;
+    public Sprite reImage;
     public Sprite resultImageS;//結果ごとの表示画像
     public Sprite resultImageA;//結果ごとの表示画像
     public Sprite resultImageB;//結果ごとの表示画像
@@ -30,33 +34,34 @@ public class Result : MonoBehaviour
     {
         if(this.resultScores == 100)
         {
-            resultRunk = "S";
-            resultComment = "サンタさんもにっこり";
-            resultImage = resultImageS;
+            rank = "S";
+            comment = "サンタさんもにっこり";
+            reImage = resultImageS;
+            
         }
         else if(this.resultScores >= 76)
         {
-            resultRunk = "A";
-            resultComment = "サンタさんには会えたかな?";
-            resultImage = resultImageA;
+            rank = "A";
+            comment = "サンタさんには会えたかな?";
+            reImage = resultImageA;
         }
         else if(this.resultScores >= 51)
         {
-            resultRunk = "B";
-            resultComment = "おしい！もう少し";
-            resultImage = resultImageB;
+            rank = "B";
+            comment = "おしい！もう少し";
+            reImage = resultImageB;
         }
         else if(this.resultScores >= 26)
         {
-            resultRunk = "C";
-            resultComment = "子供が一番ほしいプレゼントはどれかな？";
-            resultImage = resultImageC;
+            rank = "C";
+            comment = "子供が一番ほしいプレゼントはどれかな？";
+            reImage = resultImageC;
         }
         else if(this.resultScores <= 0)
         {
-            resultRunk = "D";
-            resultComment = "もう一度チャレンジしてみよう!!";
-            resultImage = resultImageD;
+            rank = "D";
+            comment = "もう一度チャレンジしてみよう!!";
+            reImage = resultImageD;
         }
     }
 
@@ -64,13 +69,19 @@ public class Result : MonoBehaviour
     void Start()
     {
         resultScores = GameDirector.scores;//ゲームディレクタークラスのスコアを受け取る
+        //resultScores = 50;
         JudeResultRunk();
-
+        this.resultRank = GameObject.Find("resultRank");
+        this.resultComment = GameObject.Find("resultComment");
+        this.resultImage = GameObject.Find("resultImage");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.resultRank.GetComponent<Text>().text = rank;
+        this.resultComment.GetComponent<Text>().text = comment;
+        resultImage.GetComponent<Image>() = reImage;
     }
 }
